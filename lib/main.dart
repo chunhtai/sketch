a450e0c3653a80e66b16d60322abf6f362981e06
Chun-Heng Tai <chtai@chtai-macbookpro.roam.corp.google.com>
2019-03-06 11:21:47 -0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,90 +1,233 @@
 import 'package:flutter/material.dart';
+import 'package:painter/painter.dart';
+import 'dart:typed_data';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  List<Offset> points = <Offset>[];
 
   @override
   Widget build(BuildContext context) {
-    final Container sketchArea = Container(
-      margin: EdgeInsets.all(1.0),
-      alignment: Alignment.topLeft,
-      color: Colors.blueGrey[50],
-      child: CustomPaint(
-        painter: Sketcher(points),
-      ),
-    );
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Sketcher'),
-      ),
-      body: GestureDetector(
-        onPanUpdate: (DragUpdateDetails details) {
-          setState(() {
-            RenderBox box = context.findRenderObject();
-            Offset point = box.globalToLocal(details.globalPosition);
-            point = point.translate(0.0, -(AppBar().preferredSize.height));
-
-            points = List.from(points)..add(point);
-          });
-        },
-        onPanEnd: (DragEndDetails details) {
-          points.add(null);
-        },
-        child: sketchArea,
-      ),
-      floatingActionButton: FloatingActionButton(
-        tooltip: 'clear Screen',
-        backgroundColor: Colors.red,
-        child: Icon(Icons.refresh),
-        onPressed: () {
-          setState(() => points.clear());
-        },
-      ),
+    return new MaterialApp(
+      title: 'Painter Example',
+      home: new ExamplePage(),
     );
   }
 }
 
-class Sketcher extends CustomPainter {
-  final List<Offset> points;
+class ExamplePage extends StatefulWidget {
+  @override
+  _ExamplePageState createState() => new _ExamplePageState();
+}
 
-  Sketcher(this.points);
+class _ExamplePageState extends State<ExamplePage> {
+
+  bool _finished;
+  PainterController _controller;
 
   @override
-  bool shouldRepaint(Sketcher oldDelegate) {
-    return oldDelegate.points != points;
+  void initState() {
+    super.initState();
+    _finished=false;
+    _controller=_newController();
   }
 
-  void paint(Canvas canvas, Size size) {
-    Paint paint = Paint()
-      ..color = Colors.black
-      ..strokeCap = StrokeCap.round
-      ..strokeWidth = 4.0;
+  PainterController _newController(){
+    PainterController controller=new PainterController();
+    controller.thickness=5.0;
+    controller.backgroundColor=Colors.green;
+    return controller;
+  }
 
-    for (int i = 0; i < points.length - 1; i++) {
-      if (points[i] != null && points[i + 1] != null) {
-        canvas.drawLine(points[i], points[i + 1], paint);
-      }
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> actions;
+    if(_finished){
+      actions = <Widget>[
+        new IconButton(
+          icon: new Icon(Icons.content_copy),
+          tooltip: 'New Painting',
+          onPressed: ()=>setState((){
+            _finished=false;
+            _controller=_newController();
+          }),
+        ),
+      ];
+    } else {
+      actions = <Widget>[
+        new IconButton(
+            icon: new Icon(Icons.undo),
+            tooltip: 'Undo',
+            onPressed:  _controller.undo
+        ),
+        new IconButton(
+            icon: new Icon(Icons.delete),
+            tooltip: 'Clear',
+            onPressed:  _controller.clear
+        ),
+        new IconButton(
+            icon: new Icon(Icons.check),
+            onPressed: ()=>_show( _controller.finish(),context)
+        ),
+      ];
+    }
+    return new Scaffold(
+      appBar: new AppBar(
+          title: const Text('Painter Example'),
+          actions:actions,
+          bottom: new PreferredSize(
+            child: new DrawBar(_controller),
+            preferredSize: new Size(MediaQuery.of(context).size.width,30.0),
+          )
+      ),
+      body: new Center(
+          child:new AspectRatio(
+              aspectRatio: 1.0,
+              child: new Painter( _controller)
+          )
+      ),
+    );
+  }
+
+  void _show(PictureDetails picture,BuildContext context){
+    setState(() {
+      _finished=true;
+    });
+    Navigator.of(context).push(
+        new MaterialPageRoute(builder: (BuildContext context){
+          return new Scaffold(
+            appBar: new AppBar(
+              title: const Text('View your image'),
+            ),
+            body: new Container(
+                alignment: Alignment.center,
+                child:new FutureBuilder<Uint8List>(
+                  future:picture.toPNG(),
+                  builder: (BuildContext context, AsyncSnapshot<Uint8List> snapshot){
+                    switch (snapshot.connectionState)
+                    {
+                      case ConnectionState.done:
+                        if (snapshot.hasError){
+                          return new Text('Error: ${snapshot.error}');
+                        }else{
+                          return Image.memory(snapshot.data);
+                        }
+                        break;
+                      default:
+                        return new Container(
+                            child:new FractionallySizedBox(
+                              widthFactor: 0.1,
+                              child: new AspectRatio(
+                                  aspectRatio: 1.0,
+                                  child: new CircularProgressIndicator()
+                              ),
+                              alignment: Alignment.center,
+                            )
+                        );
+                    }
+                  },
+                )
+            ),
+          );
+        })
+    );
+  }
+}
+
+class DrawBar extends StatelessWidget {
+
+  final PainterController _controller;
+
+  DrawBar(this._controller);
+
+  @override
+  Widget build(BuildContext context) {
+    return  new Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        new Flexible(
+            child: new StatefulBuilder(
+                builder: (BuildContext context,StateSetter setState){
+                  return new Container(
+                      child: new Slider(
+                        value:  _controller.thickness,
+                        onChanged: (double value)=>setState((){
+                          _controller.thickness=value;
+                        }),
+                        min: 1.0,
+                        max: 20.0,
+                        activeColor: Colors.white,
+                      )
+                  );
+                }
+            )
+        ),
+        new ColorPickerButton( _controller, false),
+        new ColorPickerButton( _controller, true),
+      ],
+    );
+  }
+}
+
+
+class ColorPickerButton extends StatefulWidget {
+
+  final PainterController _controller;
+  final bool _background;
+
+  ColorPickerButton(this._controller,this._background);
+
+  @override
+  _ColorPickerButtonState createState() => new _ColorPickerButtonState();
+}
+
+class _ColorPickerButtonState extends State<ColorPickerButton> {
+  @override
+  Widget build(BuildContext context) {
+    return new IconButton(
+        icon: new Icon(_iconData,color: _color),
+        tooltip: widget._background?'Change background color':'Change draw color',
+        onPressed: _pickColor
+    );
+  }
+
+  void _pickColor(){
+    Color pickerColor=_color;
+    Navigator.of(context).push(
+        new MaterialPageRoute(
+            fullscreenDialog: true,
+            builder: (BuildContext context){
+              return new Scaffold(
+                  appBar: new AppBar(
+                    title: const Text('Pick color'),
+                  ),
+                  body: new Container(
+                      alignment: Alignment.center,
+                      child: new ColorPicker(
+                        pickerColor: pickerColor,
+                        onColorChanged: (Color c)=>pickerColor=c,
+                      )
+                  )
+              );
+            }
+        )
+    ).then((_){
+      setState((){
+        _color=pickerColor;
+      });
+    });
+  }
+
+  Color get _color=>widget._background?widget._controller.backgroundColor:widget._controller.drawColor;
+
+  IconData get _iconData=>widget._background?Icons.format_color_fill:Icons.brush;
+
+  set _color(Color color){
+    if(widget._background){
+      widget._controller.backgroundColor=color;
+    } else {
+      widget._controller.drawColor=color;
     }
   }
 }
